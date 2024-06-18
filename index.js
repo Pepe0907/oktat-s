a819ad7a1edbd566c24a9c041e0782c279f26585
Pepe <keszeg1998@gmail.com>
2024-06-18 17:59:08 +0200

    homework

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 //hello world
 //hello palacsinta
+// 1.feladat
+let a 
+a = 10
+let b 
+b = 22
+let c 
+c = a + b
+// 2.feladat
+let red
+red=3
+let blue
+blue=5
+let green
+green=red
+red=blue
+blue=green
+// 3. feladat
