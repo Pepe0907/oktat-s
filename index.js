43f150b57d64be590910af2a5e1ecbaee1d2f306
Pepe <keszeg1998@gmail.com>
2024-06-18 18:30:54 +0200

    palinka

--- index.js
+++ index.js
@@ -17,3 +17,23 @@ green=red
 red=blue
 blue=green
 // 3. feladat
+let a
+a=30
+b=((a%2)===0) && ((a%5)===0)
+// 4. feladat
+let a
+a=[10,20,30,40,50]
+let b
+b= a[0]+a[1]+a[2]+a[3]+a[4]
+//5. feladat 
+let a
+a=10
+let b
+b=20
+let c
+c=30
+let number
+number={a,b,c}
+console.log (number.a,number.b,number.c)
+// Várom a többi feladatot!
+
