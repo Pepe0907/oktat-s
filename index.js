1a9c6f1f53ac77897f75e32268bf647b1acb5de6
Pepe <keszeg1998@gmail.com>
2024-10-01 10:05:18 +0200

    Hazi

--- index.js
+++ index.js
@@ -36,4 +36,76 @@ let number
 number={a,b,c}
 console.log (number.a,number.b,number.c)
 // Várom a többi feladatot!
+//Új 3. feladat
+let array=[5,6,7,8]
+let table
+table=array[0]
+array[0]=array[3]
+array[3]=table
+table=array[1]
+array[1]=array[2]
+array[2]=table
+//Ezzel elvoltam egy darabig
+//4.feladat
+let obj={}
+obj.elso=3
+obj.masodik=6
+//5.feladat
+let table
+table=obj.elso
+obj.elso=obj.masodik
+obj.masodik=table
+//6.feladat
+let tomb
+tomb=['a','b','c','d','e']
+let vol
+vol=2
+vol=tomb[vol]
+//7.feladat
+let recept = [
+    {cim: 'Palacsinta', ido: 20},
+    {cim: 'Töltöttkaposzta', ido: 60},
+      {cim: 'Vajaskenyer', ido: 10}
+];
+//8.feladat
+let array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+array[1][1] = 10
+//9.feladat
+let a=321
+let b= ((a%2)===0 && (a%5)===0)
+//10.feladat
+let array=[1, 2, 3, 4, 5
+    let sum=array[0] + array[1] + array[2] + array[3] + array[4];
+//11.feladat
+let recept='palacsinta'
+let töltelek='lekvaros'
+let serpenyö=28
+let szakacskönyv={
+    recept:recept,
+    töltelek:töltelek,
+    serpenyo:serpenyo
+}
+console.log(szakacskonyv.recept, szakacskonyv.töltelek, szakacskonyv.serpenyo)
+//12.feladat
+let lakcim={
+    iranyitoszam:'3535' ,
+    települes:'Miskolc' ,
+    utca:'Bekes utca',
+    hazszam:'1'
+}
+console.log(lakcim)
+//13.feladat
+let a = 3
+let b = 5
+let kerület = (a+b)*2
+let terület = a*b 
+//14.feladat
+let array=[1, 2, 3]
+let brray=[4, 5, 6]
+let crray=[array[0]+brray[0],array[1]+brray[1],array[2]+brray[2]]
 
+//2.fogas 1feladat
